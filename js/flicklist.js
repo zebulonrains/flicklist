24d4f1058298c2c7f23ddb052bd709aa8019a34b
Zebulon Rains <zebulonmichaelrains@gmail.com>
2016-08-03 18:09:12 +0000

    finish FlickList 1 studio

--- js/flicklist.js
+++ js/flicklist.js
@@ -8,7 +8,7 @@ var model = {
 
 var api = {
   root: "https://api.themoviedb.org/3",
-  token: "TODO" // TODO 0 put your api key here
+  token: "52285c085f90a6bd0c4e212979ce3517" 
 }
 
 
@@ -27,8 +27,11 @@ function discoverMovies(callback) {
 			console.log("We got a response from The Movie DB!");
 			console.log(response);
 			
-			// TODO 2
+			// TODO 2 (DONE)
 			// update the model, setting its .browseItems property equal to the movies we recieved in the response
+			model.browseItems = response.results;
+			console.log("here is the model:");
+			console.log(model);
 			
 			// invoke the callback function that was passed in. 
 			callback();
@@ -44,20 +47,38 @@ function discoverMovies(callback) {
 function render() {
   // TODO 7
   // clear everything from both lists
+  $("#section-watchlist ul").empty();
+  $("#section-browse ul").empty();
   
-  // TODO 6
+  // TODO 6	(DONE)
   // for each movie on the user's watchlist, insert a list item into the <ul> in the watchlist section
+  model.watchlistItems.forEach(function(movie)	{
+  	var itemView = $("<li></li>").text(movie.original_title);
+  	$("#section-watchlist ul").append(itemView);
+  });
   
   // for each movie on the current browse list, 
   model.browseItems.forEach(function(movie) {
-		// TODO 3
+		// TODO 3 (DONE)
 		// insert a list item into the <ul> in the browse section
+		var title = $("<p></p>").text(movie.original_title);
 		
-		// TODO 4
+
+		// TODO 4 (DONE)
 		// the list item should include a button that says "Add to Watchlist"
+		var button = $("<button></button").text("Add to Watchlist").click(function()	{
+			model.watchlistItems.push(movie);
+			render();
+		});
 		
-		// TODO 5
+		var itemView = $("<li></li>").append(title).append(button);
+		
+		$("#section-browse ul").append(itemView);
+		
+		// TODO 5 (DONE)
 		// when the button is clicked, this movie should be added to the model's watchlist and render() should be called again
+		
+		
   });
   
 }
